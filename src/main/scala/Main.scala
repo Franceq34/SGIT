6d69ae6dc7a00c1c894c0e959c15bd2235489434
Quentin FRANCE <quentin.france@free.fr>
2019-10-07 17:36:14 +0200

    Commands creation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,9 @@
 import java.io._
-import crypt._
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     init()
-    add("coucou.txt")
+    Commands.add("coucou.txt")
   }
 
   def init(): Unit ={
@@ -13,22 +12,5 @@ object HelloWorld {
     fm.createDir(".sgit/refs/heads")
     fm.createDir(".sgit/refs/tags")
     fm.writeFile(".sgit/HEAD.txt", "ref: refs/heads/master")
-  }
-
-  def add(path:String): Unit ={
-    //create the file manager
-    val fm:FileManager = new FileManager("/home/quentin/IdeaProjects/output/")
-    //read the file to add
-    val content = fm.readFile(path).getOrElse("")
-    //create the encryptor
-    val cr:Crypt = new CryptSHA1()
-    val cryptedContent = cr.crypt(content)
-    //write the staged files in index
-    fm.writeFile(".sgit/index", cryptedContent)
-    //create the correct directory in objects folder
-    fm.createDir(".sgit/objects/" + cryptedContent.substring(0, 2))
-    //write the staged files in the objects
-    print(fm.writeFile(".sgit/objects/" + cryptedContent.substring(0, 2) + "/" + cryptedContent.substring(2), content))
-
   }
 }
